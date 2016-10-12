adcababff020a228c73f9e3e4ea16f0a5f4fa365
Anthony Dawson <adawson@gsngames.com>
2016-10-12 15:55:13 -0400

    PY-2016.2.3 <ayd@macbook.local Create baseRefactoring.xml, Mac OS X 10_5_ copy.xml, IntelliLang.xml, WarmNeon copy.icls, project.default.xml, diff.xml, git.xml, other.xml, packages.xml, ignore.xml, find.xml, ui.lnf.xml, filetypes.xml, remote-servers.xml, code.style.schemes.xml, plugin_ui.xml, remote-server-client-libraries.xml, colors.scheme.xml, databaseDrivers.xml, conda_packages.xml, Python.xml, Default.xml, Python Unit Test.py, jdk.table.xml, jinja2 template.j2, laf.xml, Default _1_.xml, web-browsers.xml, editor.codeinsight.xml, databaseSettings.xml, docker-registry.xml, keymap.xml, certificates.xml, debugger.xml

---
+++ fileTemplates/internal/Python Unit Test.py
@@ -0,0 +1,19 @@
+import pytest
+
+class TestCase():
+    
+    def setup(self):
+    
+    def teardown(self):
+
+    def setup_class(cls):
+
+    def teardown_class(cls):
+
+    def setup_method(self, method):
+
+    def teardown_method(self, method):
+
+    def test_something(self):
+        self.assertEqual(True, False)
+
